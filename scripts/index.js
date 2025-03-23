56f8a5de7a3766053bfd80c98f98544669ccabaa
nooooo0 <55218222+nooooo0@users.noreply.github.com.>
2025-03-23 10:16:14 -0700

    Add more styling + fix div scaling

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode) {
     document.getElementById("serverTitle").innerHTML = servername;
     document.getElementById("mapName").innerHTML = "Currently playing on " + mapname;
+    document.getElementById("statusLog").style.height = window.screen.height * 0.75 + "px"; // turns out you cant really do % stuff in divs, so this is a little hacky
 };
 function log(str) {
     var ln = ">" + str;
